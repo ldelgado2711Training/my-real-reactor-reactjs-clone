081e6cb53f7698092478e3da0edec69c33ed76cf
Luis Delgado M <ldelgado2711.training@gmail.com>
2023-02-10 15:18:01 -0600

    Create the landlord contact component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -76,8 +76,7 @@ export default function Listing() {
         ))}
       </Swiper>
 
-      <div
-        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
         onClick={() => {
           navigator.clipboard.writeText(window.location.href);
           setShareLinkCopied(true);
@@ -96,7 +95,7 @@ export default function Listing() {
       )}
 
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-        <div className="w-full h-[200px] lg-[400px]">
+        <div className="w-full ">
           <p className="text-2xl font-bold mb-3 text-blue-900  ">
             {listing.name} - $
             {listing.offer
@@ -132,7 +131,7 @@ export default function Listing() {
             {listing.description}
           </p>
 
-          <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold">
+          <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold mb-6">
             <li className="flex items-center white-space-nowrap">
               <FaBed className="text-lg mr-1" />
               {+listing.bedrooms >= 1 ? "1 Bed" : `${listing.bedrooms} Beds`}
